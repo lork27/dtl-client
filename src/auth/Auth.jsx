5c5aba887738852cfa583f853c4e75dd1cc86c9f
Luis Obregon <luisitonicaragua2002@gmail.com>
2022-02-22 21:30:22 -0400

    Add changes to regiter function

--- src/auth/Auth.jsx
+++ src/auth/Auth.jsx
@@ -1,77 +1,81 @@
-import { createContext, useContext, useState } from "react";
-import { api, addAuthHeader, removeAuthHeader } from "../api";
+import { createContext, useContext, useState } from 'react'
+import { api, addAuthHeader, removeAuthHeader } from '../api'
 
-addAuthHeader();
+addAuthHeader()
 
-const AuthContext = createContext({});
+const AuthContext = createContext({})
 
 export const AuthController = (props) => {
-  const [userData, setUserData] = useState(
-    JSON.parse(localStorage.getItem("user"))
-  );
-  const [error, setError] = useState(null);
+    const [userData, setUserData] = useState(
+        JSON.parse(localStorage.getItem('user'))
+    )
+    const [error, setError] = useState(null)
 
-  //Log In function
-  const logIn = async (email, password, onSuccess) => {
-    setError(null);
-    const response = await api.post(
-      "/users/login",
-      { email, password },
-      { validateStatus: () => true }
-    );
-    if (response.status === 200) {
-      setUserData(response.data);
-      localStorage.setItem("user", JSON.stringify(response.data));
-      addAuthHeader();
-      if (onSuccess) {
-        onSuccess();
-      }
+    //Log In function
+    const logIn = async (email, password, onSuccess) => {
+        setError(null)
+        const response = await api.post(
+            '/users/login',
+            { email, password },
+            { validateStatus: () => true }
+        )
+        if (response.status === 200) {
+            setUserData(response.data)
+            localStorage.setItem('user', JSON.stringify(response.data))
+            addAuthHeader()
+            if (onSuccess) {
+                onSuccess()
+            }
+        }
+        if (response.status === 400) {
+            setError(response.data.error)
+            console.log(error)
+        }
     }
-    if (response.status === 400) {
-      setError(response.data.error);
-    }
-  };
 
-  const registerUser = async ({
-    name,
-    email,
-    password,
-    confirmPassword,
-    onSuccess,
-  }) => {
-    setError(null);
-    const response = await api.post(
-      "/users/new",
-      { name, email, password, confirmPassword },
-      { validateStatus: () => true }
-    );
-    if (response.status === 201) {
-      setUserData(response.data);
-      localStorage.setItem("user", JSON.stringify(response.data));
-      addAuthHeader();
-      if (onSuccess) {
-        onSuccess();
-      }
-    }
-    if (response.status === 400) {
-      setError(response.data.error);
-    }
-  };
+    // Register user function
+    const registerUser = async ({
+        name,
+        email,
+        password,
+        passwordConfirm,
+        onSuccess,
+    }) => {
+        setError(null)
+        const response = await api.post(
+            '/users/new',
+            { name, email, password, passwordConfirm },
+            { validateStatus: () => true }
+        )
+        console.log(name, email, password, passwordConfirm)
 
-  const logOut = () => {
-    localStorage.removeItem("user");
-    removeAuthHeader();
-    setUserData(null);
-  };
-  return (
-    <AuthContext.Provider
-      value={{ userData, logIn, error, logOut, registerUser }}
-    >
-      {props.children}
-    </AuthContext.Provider>
-  );
-};
+        if (response.status === 201) {
+            setUserData(response.data)
+            localStorage.setItem('user', JSON.stringify(response.data))
+            addAuthHeader()
+            if (onSuccess) {
+                onSuccess()
+            }
+        }
+        if (response.status === 400) {
+            setError(response.data.error)
+        }
+    }
+
+    const logOut = () => {
+        localStorage.removeItem('user')
+        removeAuthHeader()
+        setUserData(null)
+    }
+    return (
+        <AuthContext.Provider
+            value={{ userData, logIn, error, logOut, registerUser }}
+        >
+            {props.children}
+        </AuthContext.Provider>
+    )
+}
 
 export const useAuth = () => {
-  return useContext(AuthContext);
-};
+    return useContext(AuthContext)
+}
