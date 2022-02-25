b6a3cbbc5d6921df376cecf30099c6af32a78dfc
lork27 <g.lorca.lamadrid@gmail.com>
2022-02-25 16:57:55 -0400

    added first version of the guest landing page

---
+++ src/pages/GuestPage.jsx
@@ -0,0 +1,152 @@
+import { useAuth } from '../auth/Auth'
+import * as React from 'react'
+import AppBar from '@mui/material/AppBar'
+import Button from '@mui/material/Button'
+import CameraIcon from '@mui/icons-material/PhotoCamera'
+import Card from '@mui/material/Card'
+import CardActions from '@mui/material/CardActions'
+import CardContent from '@mui/material/CardContent'
+import CardMedia from '@mui/material/CardMedia'
+import CssBaseline from '@mui/material/CssBaseline'
+import Grid from '@mui/material/Grid'
+import Stack from '@mui/material/Stack'
+import Box from '@mui/material/Box'
+import Toolbar from '@mui/material/Toolbar'
+import Typography from '@mui/material/Typography'
+import Container from '@mui/material/Container'
+import Link from '@mui/material/Link'
+import { createTheme, ThemeProvider } from '@mui/material/styles'
+import { Link as RouterDomLink } from 'react-router-dom'
+
+//This is the page that will show when no user is logged in
+//It will look kinda like this...
+//https://cdn.discordapp.com/attachments/943515943760457738/943520909011091496/IMG_20220216_105449.jpg
+function Copyright() {
+    return (
+        <Typography variant="body2" color="text.secondary" align="center">
+            {'Copyright © '}
+            <Link color="inherit" href="https://mui.com/">
+                Your Website
+            </Link>{' '}
+            {new Date().getFullYear()}
+            {'.'}
+        </Typography>
+    )
+}
+
+const tutorsCards = [1, 2, 3]
+
+const theme = createTheme()
+export const GuestPage = () => {
+    const { userData } = useAuth()
+    return (
+        <ThemeProvider theme={theme}>
+            <CssBaseline />
+            <main>
+                {/* Hero unit */}
+                <Box
+                    sx={{
+                        bgcolor: 'background.paper',
+                        pt: 8,
+                        pb: 6,
+                    }}
+                >
+                    <Container maxWidth="sm">
+                        <Typography
+                            component="h1"
+                            variant="h2"
+                            align="center"
+                            color="text.primary"
+                            gutterBottom
+                        >
+                            DTL?
+                        </Typography>
+                        <Typography
+                            variant="h5"
+                            align="center"
+                            color="text.secondary"
+                            paragraph
+                        >
+                            The social network that connects people that want to
+                            teach and learn
+                        </Typography>
+                        <Stack
+                            sx={{ pt: 4 }}
+                            direction="row"
+                            spacing={2}
+                            justifyContent="center"
+                        >
+                            <Link component={RouterDomLink} to="/register">
+                                <Button variant="contained">
+                                    Become a Student
+                                </Button>
+                            </Link>
+                            <Link component={RouterDomLink} to="/register">
+                                <Button variant="contained">
+                                    Become a Tutor
+                                </Button>
+                            </Link>
+                        </Stack>
+                    </Container>
+                </Box>
+                <Container sx={{ py: 8 }} maxWidth="md">
+                    {/* End hero unit */}
+                    <Grid container spacing={4}>
+                        {tutorsCards.map((card) => (
+                            <Grid item key={card} xs={12} sm={6} md={4}>
+                                <Card
+                                    sx={{
+                                        height: '100%',
+                                        display: 'flex',
+                                        flexDirection: 'column',
+                                    }}
+                                >
+                                    <CardMedia
+                                        component="img"
+                                        sx={{
+                                            // 16:9
+                                            pt: '56.25%',
+                                        }}
+                                        image="https://source.unsplash.com/random"
+                                        alt="random"
+                                    />
+                                    <CardContent sx={{ flexGrow: 1 }}>
+                                        <Typography
+                                            gutterBottom
+                                            variant="h5"
+                                            component="h2"
+                                        >
+                                            Heading
+                                        </Typography>
+                                        <Typography>
+                                            This is a media card. You can use
+                                            this section to describe the
+                                            content.
+                                        </Typography>
+                                    </CardContent>
+                                    <CardActions>
+                                        <Button size="small">View more</Button>
+                                    </CardActions>
+                                </Card>
+                            </Grid>
+                        ))}
+                    </Grid>
+                </Container>
+            </main>
+            {/* Footer */}
+            <Box sx={{ bgcolor: 'background.paper', p: 6 }} component="footer">
+                <Typography
+                    variant="subtitle1"
+                    align="center"
+                    color="text.secondary"
+                    component="p"
+                >
+                    webapp developed by Luis Obregon and Guillermo Lorca for
+                    their foundation's year portfolio project
+                </Typography>
+                <Copyright />
+            </Box>
+            {/* End footer */}
+        </ThemeProvider>
+    )
+}
---
+++ src/pages/LandingPage.jsx
@@ -0,0 +1,18 @@
+import { useAuth } from '../auth/Auth'
+
+//This page will be the user landingpage it needs to looks something like this...
+//https://cdn.discordapp.com/attachments/943515943760457738/943526139106820166/IMG_20220216_111546.jpg
+export const LandingPage = () => {
+    const { userData } = useAuth()
+    console.log(userData)
+    return (
+        <div>
+            <h1>
+                This is the user landing page, you are already down to learn
+            </h1>
+            <p>
+                Welcome <b>{userData.username}</b>
+            </p>
+        </div>
+    )
+}
