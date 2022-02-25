aecfae18e5843a62ceab21532340c33edd56829e
lork27 <g.lorca.lamadrid@gmail.com>
2022-02-25 17:24:13 -0400

    getting url from env variable so it works in both production and development eviroments

--- src/api/index.js
+++ src/api/index.js
@@ -1,16 +1,21 @@
-import axios from "axios";
+import axios from 'axios'
+
+const URL =
+    process.env.NODE_ENV === 'production'
+        ? 'https://us-central1-dtl-mvp.cloudfunctions.net/api/'
+        : 'http://localhost:5000/dtl-mvp/us-central1/api/'
 
 export const api = axios.create({
-  baseURL: "http://localhost:5000/dtl-mvp/us-central1/api/",
-});
+    baseURL: URL,
+})
 
 export const addAuthHeader = () => {
-  const token = JSON.parse(localStorage.getItem("user"))?.token;
-  if (token) {
-    api.defaults.headers.common["Authorization"] = `Bearer ${token}`;
-  }
-};
+    const token = JSON.parse(localStorage.getItem('user'))?.token
+    if (token) {
+        api.defaults.headers.common['Authorization'] = `Bearer ${token}`
+    }
+}
 
 export const removeAuthHeader = () => {
-  delete api.defaults.headers.common["Authorization"];
-};
+    delete api.defaults.headers.common['Authorization']
+}
