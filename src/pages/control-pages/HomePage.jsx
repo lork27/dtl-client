69af7488e532013c7b6963ba9050aaaaccda1dbe
lork27 <g.lorca.lamadrid@gmail.com>
2022-02-27 18:37:24 -0400

    updated homepage logic

--- src/pages/control-pages/HomePage.jsx
+++ src/pages/control-pages/HomePage.jsx
@@ -6,3 +6,8 @@ export const HomePage = () => {
     console.log(userData)
     return <div>{userData ? <LandingPage /> : <GuestPage />}</div>
 }
+
+//This whole approach of using one individual controller for each time
+// I need to protect a route is not scalable
+// If we want to scale this project we should use this approach
+// https://youtu.be/0x8Dap2EIVE
